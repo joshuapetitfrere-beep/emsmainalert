8cac92d874b99513f9629e8a71213feccf0a325a
Joshua Petit Frere <joshuapetitfrere@gmail.com>
2026-03-11 14:33:37 -0400

    Updated server with status endpoint and alert IDs

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from fastapi import FastAPI
-import os 
-app = FastAPI()
-API_TOKEN = os.getenv("API_TOKEN")
-ENV = os.getenv("ENV", "testing") #default to testing 
-
-@app.get("/")
-def root():
-    return {"status": "EMS Backend Is Running"}
 
